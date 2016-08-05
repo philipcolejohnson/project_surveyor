5a9a44ce15fb28c747ada51cc0f9897cbca8e116
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-05 15:48:05 -0700

    Added Questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,55 @@
 class QuestionsController < ApplicationController
+  def index
+    @question = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = "Your question has been created!"
+      redirect_to @question
+    else
+      flash.now[:danger] = "Your question could not be created :("
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update
+      flash[:success] = "Your question has been updated!"
+      redirect_to @question
+    else
+      flash.now[:danger] = "Your question could not be updated :("
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy
+      flash[:success] = "Your question has been destroyed!"
+      redirect_to questions_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text, :type, :required)
+  end
 end
